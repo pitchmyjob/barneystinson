a5489e609ad5159582a640665c725710dbdff84d
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-01-18 13:46:06 +0100

    Add noqa comment where needed

--- barneystinson/apps/core/storages.py
+++ barneystinson/apps/core/storages.py
@@ -3,5 +3,5 @@ from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
 
 
-StaticStorage = lambda: S3BotoStorage(location=settings.STATICFILES_LOCATION)
-MediaStorage = lambda: S3BotoStorage(location=settings.MEDIAFILES_LOCATION)
+StaticStorage = lambda: S3BotoStorage(location=settings.STATICFILES_LOCATION)  # noqa
+MediaStorage = lambda: S3BotoStorage(location=settings.MEDIAFILES_LOCATION)  # noqa
