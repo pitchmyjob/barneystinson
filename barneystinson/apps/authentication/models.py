d198ec7c826955c285d5e59414c8cb6ab4a5b7ce
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-01-19 15:02:51 +0100

    Remove username field from User model

--- barneystinson/apps/authentication/models.py
+++ barneystinson/apps/authentication/models.py
@@ -6,6 +6,7 @@ from apps.core.fields import ImageField
 
 
 class User(AbstractUser):
+    username = False
     email = models.EmailField(_('email address'), unique=True)
     photo = ImageField(_('photo'), blank=True, default='user/photo/default.jpg')
     phone = models.CharField(_('numéro de téléphone'), max_length=250, default='')
@@ -13,7 +14,7 @@ class User(AbstractUser):
     pro = models.ForeignKey('pro.Pro', blank=True, null=True, verbose_name=_('pro'))
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = _('user')
