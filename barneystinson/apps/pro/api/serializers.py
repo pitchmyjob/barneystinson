88697ddb28e23c733f560b27ab100f95d09302c7
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-01-23 11:50:12 +0100

    Remove old APIView URLs and serializers related to auth and register

--- barneystinson/apps/pro/api/serializers.py
+++ barneystinson/apps/pro/api/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 
-from apps.authentication.api.serializers import UserRegisterSerializer
-
 from ..models import Pro
 
 
@@ -9,14 +7,3 @@ class ProSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pro
         fields = '__all__'
-
-
-class ProRegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pro
-        fields = ('company',)
-
-
-class ProRegisterSerializer(serializers.Serializer):
-    pro = ProRegisterSerializer()
-    user = UserRegisterSerializer()
