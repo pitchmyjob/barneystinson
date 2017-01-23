ac4f3b6d7012ba82ce1fb94669ea8dd7b11ec960
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-01-23 11:59:11 +0100

    Adds login and register views for Applicant and Pro

--- barneystinson/apps/authentication/api/urls.py
+++ barneystinson/apps/authentication/api/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 
-from .views import AuthProLoginAPIView
+from .views import AuthRegisterApplicantAPIView, AuthLoginApplicantAPIView, AuthRegisterProAPIView, AuthLoginProAPIView
 
 
 urlpatterns = [
-    url(r'^auth/pro/login', AuthProLoginAPIView.as_view()),
+    url(r'^auth/applicant/register', AuthRegisterApplicantAPIView.as_view()),
+    url(r'^auth/applicant/login', AuthLoginApplicantAPIView.as_view()),
+    url(r'^auth/pro/register', AuthRegisterProAPIView.as_view()),
+    url(r'^auth/pro/login', AuthLoginProAPIView.as_view()),
 ]
