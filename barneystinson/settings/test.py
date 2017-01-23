8957859c25641b97b59ccbbf7b406726d2b0cca7
yannistannier <tannier.yannis@gmail.com>
2017-01-23 18:44:51 +0100

    add database in settings.test

--- barneystinson/settings/test.py
+++ barneystinson/settings/test.py
@@ -5,3 +5,13 @@ INSTALLED_APPS += [
     # Thirds apps
     'django_jenkins',
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': get_env_variable('DJANGO_DB_DEV_HOST'),
+        'NAME': get_env_variable('DJANGO_DB_DEV_NAME'),
+        'USER': get_env_variable('DJANGO_DB_DEV_USER'),
+        'PASSWORD': get_env_variable('DJANGO_DB_DEV_PASSWORD'),
+    }
+}
