76a0fefe857f4ea721219c36689833690ca3b04d
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-01-24 16:21:10 +0100

    Adds some generic tests

--- barneystinson/apps/core/api/tests.py
+++ barneystinson/apps/core/api/tests.py
@@ -1,3 +1,5 @@
+import unittest
+
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
@@ -7,11 +9,14 @@ from django.utils.six import text_type
 
 
 class BaseAPITestCase(APITestCase):
+    LIST_SUFFIX_URL = '-list'
+    DETAIL_SUFFIX_URL = '-detail'
+    LOOKUP_FIELD = 'pk'
+
     base_name = None
     factory_class = None
-    user_factory_class = None
     serializer_class = None
-    lookup_field = 'pk'
+    user_factory_class = None
 
     def get_factory_class(self):
         return getattr(self, 'factory_class')
@@ -37,23 +42,19 @@ class BaseAPITestCase(APITestCase):
                                        'or override the `get_serializer_class()` method.' % self.__class__.__name__)
         return self.serializer_class
 
-    def get_serializer_context(self):
-        return {}
-
     def get_serializer(self, *args, **kwargs):
         serializer_class = self.get_serializer_class()
-        kwargs['context'] = self.get_serializer_context()
         return serializer_class(*args, **kwargs)
 
-    def setUp(self):
-        if self.user_factory_class:
-            self.user = self.generate_user()
-            self.client.force_authenticate(self.user)
+    def authenticate_user(self):
+        self.user = self.generate_user()
+        self.client.force_authenticate(self.user)
 
 
 class ListAPITestCaseMixin(object):
-    LIST_SUFFIX_URL = '-list'
-    pagination_results_field = None
+    nb_objects_to_generate = 5
+    list_status_code_only = False
+    list_expected_status_code = status.HTTP_200_OK
 
     def get_list_url(self):
         return reverse(self.base_name + self.LIST_SUFFIX_URL)
@@ -61,53 +62,107 @@ class ListAPITestCaseMixin(object):
     def get_list_response(self, **kwargs):
         return self.client.get(self.get_list_url(), **kwargs)
 
-    def test_list_status_code(self, **kwargs):
-        response = self.get_list_response(**kwargs)
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+    def generate_objects(self, **kwargs):
+        return [self.generate_object(**kwargs) for i in range(self.nb_objects_to_generate)]
 
-    def test_list_nb_returned_data(self, **kwargs):
-        data = [self.generate_object() for i in range(5)]
-        response = self.get_list_response(**kwargs)
+    def test_list_status_code(self):
+        response = self.get_list_response()
+        self.assertEqual(response.status_code, self.list_expected_status_code)
+
+    def test_list_nb_data_returned(self):
+        if self.list_status_code_only:
+            return unittest.skip('`list_status_code_only` attribut set to True')
+
+        data = self.generate_objects()
+        response = self.get_list_response()
         self.assertTrue(len(response.data) == len(data))
 
-    def test_list_content_returned_data(self, **kwargs):
-        data = [self.generate_object() for i in range(5)]
-        response = self.get_list_response(**kwargs)
+    def test_list_content_returned(self):
+        if self.list_status_code_only:
+            return unittest.skip('`list_status_code_only` attribut set to True')
+
+        data = self.generate_objects()
+        response = self.get_list_response()
         serializer = self.get_serializer(data, many=True)
 
-        identical = len(response.data) == len(serializer.data)
-        if identical:
-            for obj in serializer.data:
-                if obj not in response.data:
-                    identical = False
-        self.assertEqual(identical, True)
+        is_identical = len(response.data) == len(serializer.data)
+        if is_identical:
+            for obj in response.data:
+                if obj not in serializer.data:
+                    is_identical = False
+                    break
+        self.assertTrue(is_identical)
 
 
 class RetrieveAPITestCaseMixin(object):
-    RETRIEVE_SUFFIX_URL = '-detail'
-
-    def get_retrieve_objet_id(self):
-        return getattr(self.object, self.lookup_field)
+    retrieve_status_code_only = False
+    retrieve_expected_status_code = status.HTTP_200_OK
 
     def get_retrieve_url(self):
-        object_id = self.get_retrieve_objet_id()
-        return reverse(self.base_name + self.RETRIEVE_SUFFIX_URL, args=[text_type(object_id)])
+        object_id = getattr(self.object, self.LOOKUP_FIELD)
+        return reverse(self.base_name + self.DETAIL_SUFFIX_URL, args=[text_type(object_id)])
 
     def get_retrieve_response(self, **kwargs):
         return self.client.get(self.get_retrieve_url(), **kwargs)
 
-    def generate_retrieve_object(self):
-        return self.generate_object()
-
-    def test_retrieve_status_code(self, **kwargs):
+    def test_retrieve_status_code(self):
         self.object = self.generate_object()
+        response = self.get_retrieve_response()
+        self.assertEqual(response.status_code, self.retrieve_expected_status_code)
 
-        response = self.get_retrieve_response(**kwargs)
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+    def test_retrieve_content_returned(self):
+        if self.retrieve_status_code_only:
+            return unittest.skip('`retrieve_status_code_only` attribut set to True')
 
-    def test_retrieve_returned_data(self, **kwargs):
-        self.object = self.generate_retrieve_object()
-
+        self.object = self.generate_object()
         serializer = self.get_serializer(self.object)
-        response = self.get_retrieve_response(**kwargs)
+        response = self.get_retrieve_response()
         self.assertEqual(response.data, serializer.data)
+
+
+class CreateAPITestCaseMixin(object):
+    def get_create_data(self):
+        pass
+
+    def get_create_url(self):
+        pass
+
+    def get_create_response(self, data=None, **kwargs):
+        pass
+
+    def get_lookup_from_response(self, data):
+        pass
+
+    def test_create(self, data=None, **kwargs):
+        pass
+
+
+class UpdateAPITestCaseMixin(object):
+    def get_update_url(self):
+        pass
+
+    def get_update_response(self, data=None, results=None, use_path=None, **kwargs):
+        pass
+
+    def get_update_data(self):
+        pass
+
+    def get_update_results(self, data=None):
+        pass
+
+    def get_relationship_value(self, related_obj, key):
+        pass
+
+    def test_update(self, data=None, results=None, use_patch=None, **kwargs):
+        pass
+
+
+class DestroyAPITestCaseMixin(object):
+    def get_destroy_url(self):
+        pass
+
+    def get_destroy_response(self, **kwargs):
+        pass
+
+    def test_destroy(self, **kwargs):
+        pass
--- barneystinson/apps/pro/api/tests.py
+++ barneystinson/apps/pro/api/tests.py
@@ -1,19 +1,38 @@
-from apps.core.api import tests
+from rest_framework import status
 
 from apps.authentication.factories import UserProFactory
+from apps.core.api import tests
 
 from .serializers import ProSerializer
 from ..factories import ProFactory
 
 
-class ProAPITestCase(tests.RetrieveAPITestCaseMixin, tests.BaseAPITestCase):
+class ProAPITestCase(tests.RetrieveAPITestCaseMixin,
+                     tests.UpdateAPITestCaseMixin,
+                     tests.DestroyAPITestCaseMixin,
+                     tests.BaseAPITestCase):
     base_name = 'pro'
     factory_class = ProFactory
     user_factory_class = UserProFactory
     serializer_class = ProSerializer
+    retrieve_status_code_only = True
+    retrieve_expected_status_code = status.HTTP_401_UNAUTHORIZED
 
-    def get_retrieve_objet_id(self):
-        return self.user.pro.pk
+    def test_authenticated_only_related_pro_status_code(self):
+        self.authenticate_user()
+        self.object = self.generate_object()
+        response = self.get_retrieve_response()
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def generate_retrieve_object(self):
-        return self.user.pro
+    def test_authenticated_realted_pro_status_code(self):
+        self.authenticate_user()
+        self.object = self.user.pro
+        response = self.get_retrieve_response()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_authenticated_content_returned(self):
+        self.authenticate_user()
+        self.object = self.user.pro
+        serializer = self.get_serializer(self.object)
+        response = self.get_retrieve_response()
+        self.assertEqual(response.data, serializer.data)
