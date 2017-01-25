e7b8a0b8340ab6c2e7601435cbf2a523da180cc4
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-01-25 19:12:41 +0100

    Uses IsProUser permission en ViewSet

--- barneystinson/apps/pro/api/views.py
+++ barneystinson/apps/pro/api/views.py
@@ -1,26 +1,24 @@
-from rest_framework import mixins
+from rest_framework import mixins, permissions
 from rest_framework.viewsets import GenericViewSet
 
 from apps.authentication.models import User
-from apps.core.api.mixins import IsAuthenticatedMixin, IsActiveDestroyMixin
+from apps.core.api.mixins import IsActiveDestroyMixin
 
-from ..models import Pro
+from .permissions import IsProUser
 from .serializers import ProSerializer
+from ..models import Pro
 
 
-class ProViewSet(IsAuthenticatedMixin,
-                 IsActiveDestroyMixin,
+class ProViewSet(IsActiveDestroyMixin,
                  mixins.RetrieveModelMixin,
                  mixins.UpdateModelMixin,
                  mixins.DestroyModelMixin,
                  GenericViewSet):
+    permission_classes = [permissions.IsAuthenticated, IsProUser]
     serializer_class = ProSerializer
 
     def get_queryset(self):
-        pro = self.request.user.pro
-        if pro:
-            return Pro.objects.filter(pk=pro.pk, is_active=True)
-        return Pro.objects.none()
+        return Pro.objects.filter(pk=self.request.user.pro.pk, is_active=True)
 
     def perform_destroy(self, instance):
         User.objects.filter(pro=instance).update(is_active=False)
