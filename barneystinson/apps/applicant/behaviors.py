c40f52c58e194985d95d933c8d3be60d1f077572
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-01-26 18:23:15 +0100

    Removes ApplicantRelated model mixin and readd all foreignkey with a related_name

--- barneystinson/apps/applicant/behaviors.py
+++ barneystinson/apps/applicant/behaviors.py
@@ -2,13 +2,6 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-class ApplicantRelated(models.Model):
-    applicant = models.ForeignKey('applicant.Applicant', verbose_name=_('postulant'))
-
-    class Meta:
-        abstract = True
-
-
 class StartEndDate(models.Model):
     date_start = models.DateField(_('date de début'), null=True, blank=True)
     date_end = models.DateField(_('date de fin'), null=True, blank=True)
