0a376b963cb697f8a84f1cf39e0534c5dca6f094
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-01-27 13:11:23 +0000

    Adds an API endpoint to get/update current logged in user

--- barneystinson/apps/authentication/api/urls.py
+++ barneystinson/apps/authentication/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
-from .views import AuthRegisterApplicantAPIView, AuthLoginApplicantAPIView, AuthRegisterProAPIView, AuthLoginProAPIView
+from .views import (AuthRegisterApplicantAPIView, AuthLoginApplicantAPIView, AuthRegisterProAPIView,
+                    AuthLoginProAPIView, AuthMeAPIView)
 
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     url(r'^auth/applicant/login', AuthLoginApplicantAPIView.as_view(), name='applicant-login'),
     url(r'^auth/pro/register', AuthRegisterProAPIView.as_view(), name='pro-register'),
     url(r'^auth/pro/login', AuthLoginProAPIView.as_view(), name='pro-login'),
+    url(r'^auth/me', AuthMeAPIView.as_view(), name='me'),
 ]
