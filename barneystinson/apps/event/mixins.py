4f929877bb1a38bd50de07863fb5ee90a4132165
Yannis <yannis@sd-91171.dedibox.fr>
2017-01-27 20:58:28 +0100

    EventtApplicant correct run_flake8

--- barneystinson/apps/event/mixins.py
+++ barneystinson/apps/event/mixins.py
@@ -1,8 +1,5 @@
-import inspect
-
-from rest_framework.renderers import JSONRenderer
-
 from .event import ApplicantEvent
+
 
 class EventApplicantMixin(object):
 
@@ -12,14 +9,26 @@ class EventApplicantMixin(object):
             del payload['applicant']
         object = serializer.save()
         payload['id'] = object.id
-        ApplicantEvent(id=self.request.user.applicant.id, payload=payload, event="add_" + self.event_type).save()
+        ApplicantEvent(
+            id=self.request.user.applicant.id,
+            payload=payload,
+            event="add_" + self.event_type
+        ).save()
 
     def perform_update(self, serializer):
         payload = serializer.validated_data
         payload['id'] = self.get_object().id
-        ApplicantEvent(id=self.request.user.applicant.id, payload=payload, event="edit_"+self.event_type).save()
+        ApplicantEvent(
+            id=self.request.user.applicant.id,
+            payload=payload,
+            event="edit_" + self.event_type
+        ).save()
         super(EventApplicantMixin, self).perform_update(serializer)
 
     def perform_destroy(self, serializer):
-        ApplicantEvent(id=self.request.user.applicant.id, payload={'id' : self.get_object().id}, event="delete_"+self.event_type).save()
+        ApplicantEvent(
+            id=self.request.user.applicant.id,
+            payload={'id': self.get_object().id},
+            event="delete_" + self.event_type
+        ).save()
         super(EventApplicantMixin, self).perform_destroy(serializer)
