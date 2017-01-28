ef95201b927adbf6503d6cabc29310ef32de7dab
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-01-28 10:39:31 +0000

    Rewrites URLs to use pro and applicant base url

--- barneystinson/apps/candidacy/api/urls.py
+++ barneystinson/apps/candidacy/api/urls.py
@@ -7,29 +7,17 @@ from .views import (CandidacyProListAPIView, CandidacyProRetrieveAPIView, Candid
 
 
 urlpatterns = [
-    url(r'^procandidacies/(?P<pk>\d+)/list', CandidacyProListAPIView.as_view(), name='procandidacy-detail'),
-    url(r'^procandidacies/(?P<pk>\d+)/approve', CandidacyProApproveAPIView.as_view(), name='procandidacy-approve'),
-    url(r'^procandidacies/(?P<pk>\d+)', CandidacyProRetrieveAPIView.as_view(), name='procandidacy-detail'),
-    url(r'^procandidacies/request', CandidacyProRequestAPIView.as_view(), name='procandidacy-request'),
-    url(
-        r'^procandidacies/(?P<pk>\d+)/disapprove',
-        CandidacyProDisapproveAPIView.as_view(),
-        name='procandidacy-disapprove'
-    ),
-    url(
-        r'^applicantcandidacies/(?P<pk>\d+)/like',
-        CandidacyApplicantLikeAPIView.as_view(),
-        name='applicantcandidacy-like'
-    ),
-    url(
-        r'^applicantcandidacies/(?P<pk>\d+)/postulate',
-        CandidacyApplicantPostulateAPIView.as_view(),
-        name='applicantcandidacy-postulate'
-    ),
-    url(
-        r'^applicantcandidacies/(?P<pk>\d+)',
-        CandidacyApplicantRetrieveAPIView.as_view(),
-        name='applicantcandidacy-detail'
-    ),
-    url(r'^applicantcandidacies', CandidacyApplicantListAPIView.as_view(), name='applicantcandidacy-detail'),
+    url(r'^pro/candidacies/(?P<job_pk>\d+)', CandidacyProListAPIView.as_view(), name='procandidacy-list'),
+    url(r'^pro/candidacy/(?P<pk>\d+)/approve', CandidacyProApproveAPIView.as_view(), name='procandidacy-approve'),
+    url(r'^pro/candidacy/(?P<pk>\d+)/disapprove', CandidacyProDisapproveAPIView.as_view(),
+        name='procandidacy-disapprove'),
+    url(r'^pro/candidacy/(?P<pk>\d+)', CandidacyProRetrieveAPIView.as_view(), name='procandidacy-detail'),
+    url(r'^pro/candidacy/request', CandidacyProRequestAPIView.as_view(), name='procandidacy-request'),
+    url(r'^applicant/candidacies', CandidacyApplicantListAPIView.as_view(), name='applicantcandidacy-list'),
+    url(r'^applicant/candidacy/(?P<pk>\d+)/like', CandidacyApplicantLikeAPIView.as_view(),
+        name='applicantcandidacy-like'),
+    url(r'^applicant/candidacy/(?P<pk>\d+)/postulate', CandidacyApplicantPostulateAPIView.as_view(),
+        name='applicantcandidacy-postulate'),
+    url(r'^applicant/candidacy/(?P<pk>\d+)', CandidacyApplicantRetrieveAPIView.as_view(),
+        name='applicantcandidacy-detail'),
 ]
