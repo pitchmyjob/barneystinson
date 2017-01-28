125b422bd0fbf6acd57bf2ae3aac5fd10093e5b5
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-01-28 10:37:53 +0000

    Adds two mixins to handle Pro and Applicant candidacies views

---
+++ barneystinson/apps/candidacy/api/mixins.py
@@ -0,0 +1,47 @@
+from rest_framework import permissions
+
+from django.db.models import Q
+
+from apps.applicant.api.permissions import IsApplicantUser
+from apps.pro.api.permissions import IsProUser
+
+from .serializers import (CandidacyProReadSerializer, CandidacyProRequestSerializer, CandidacyProApproveSerializer,
+                          CandidacyProDisapproveSerializer, CandidacyApplicantReadSerializer,
+                          CandidacyApplicantLikeSerializer, CandidacyApplicantVideoSerializer)
+from ..models import Candidacy
+
+
+class CandidacyMixin(object):
+    def get_serializer_class(self):
+        url_name = self.request.resolver_match.url_name
+        return self.serializer_class.get(url_name)
+
+
+class CandidacyProMixin(CandidacyMixin):
+    permission_classes = [permissions.IsAuthenticated, IsProUser]
+    serializer_class = {
+        'procandidacy-list': CandidacyProReadSerializer,
+        'procandidacy-detail': CandidacyProReadSerializer,
+        'procandidacy-request': CandidacyProRequestSerializer,
+        'procandidacy-approve': CandidacyProApproveSerializer,
+        'procandidacy-disapprove': CandidacyProDisapproveSerializer,
+    }
+
+    def get_queryset(self):
+        qs_filter = {}
+        if 'job_pk' in self.kwargs:
+            qs_filter = {'job': self.kwargs.get('job_pk')}
+        return Candidacy.objects.filter(~Q(status=Candidacy.MATCHING), job__pro=self.request.user.pro, **qs_filter)
+
+
+class CandidacyApplicantMixin(CandidacyMixin):
+    permission_classes = [permissions.IsAuthenticated, IsApplicantUser]
+    serializer_class = {
+        'applicantcandidacy-list': CandidacyApplicantReadSerializer,
+        'applicantcandidacy-detail': CandidacyApplicantReadSerializer,
+        'applicantcandidacy-like': CandidacyApplicantLikeSerializer,
+        'applicantcandidacy-postulate': CandidacyApplicantVideoSerializer,
+    }
+
+    def get_queryset(self):
+        return Candidacy.objects.filter(applicant=self.request.user.applicant)
