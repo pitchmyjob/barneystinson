2c353bac34e0058c12d8a028625117aa5941b648
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-01-30 12:12:06 +0000

    Adds DjangoModelPermissions to check delete_pro permission

--- barneystinson/apps/pro/api/views.py
+++ barneystinson/apps/pro/api/views.py
@@ -1,16 +1,17 @@
-from rest_framework import permissions
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from apps.authentication.models import User
 from apps.core.api.mixins import IsActiveDestroyMixin
 
 from .permissions import IsProUser
 from .serializers import ProSerializer
+from ..models import Pro
 
 
 class ProMeAPIView(IsActiveDestroyMixin, generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [permissions.IsAuthenticated, IsProUser]
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions, IsProUser]
     serializer_class = ProSerializer
+    queryset = Pro.objects.none()
 
     def perform_destroy(self, instance):
         User.objects.filter(pro=instance).update(is_active=False)
