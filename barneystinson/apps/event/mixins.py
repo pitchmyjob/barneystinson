61d1bb2978c5ebea84d2b8ade6bdcef30f654dce
Yannis <yannis@sd-91171.dedibox.fr>
2017-01-31 17:05:34 +0100

    edit event/mixins and add control new applicant

--- barneystinson/apps/event/mixins.py
+++ barneystinson/apps/event/mixins.py
@@ -5,19 +5,30 @@ class EventApplicantMixin(object):
 
     def perform_create(self, serializer):
         object = serializer.save()
-        if hasattr(self.request.user, 'applicant'):
+        run = False
+
+        if self.request.user.is_authenticated():
+            if self.request.user.is_applicant:
+                run = True
+                applicant = self.request.user.applicant
+        else:
+            if self.event_type == "applicant":
+                run = True
+                applicant = object.applicant
+
+        if run:
             payload = dict(serializer.validated_data)
             if 'applicant' in payload:
                 del payload['applicant']
             payload['id'] = object.id
             ApplicantEvent(
-                id=self.request.user.applicant.id,
+                id=applicant.id,
                 payload=payload,
                 event="add_" + self.event_type
             ).save()
 
     def perform_update(self, serializer):
-        if hasattr(self.request.user, 'applicant'):
+        if self.request.user.is_applicant:
             payload = serializer.validated_data
             payload['id'] = self.get_object().id
             ApplicantEvent(
@@ -28,7 +39,7 @@ class EventApplicantMixin(object):
         super(EventApplicantMixin, self).perform_update(serializer)
 
     def perform_destroy(self, serializer):
-        if hasattr(self.request.user, 'applicant'):
+        if self.request.user.is_applicant:
             ApplicantEvent(
                 id=self.request.user.applicant.id,
                 payload={'id': self.get_object().id},
