0a7e77e1edcc9a3c9f14d2116c5a24971f19a36a
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-08 16:53:20 +0000

    Adds Meta and verbose_name + unique together

--- barneystinson/apps/message/models.py
+++ barneystinson/apps/message/models.py
@@ -9,9 +9,18 @@ class CandidacyMessage(models.Model):
     message = models.TextField(_('message'))
     created = models.DateTimeField(_('date de création'), auto_now_add=True)
 
+    class Meta:
+        verbose_name = _('message')
+        verbose_name_plural = _('messages')
+
 
 class CandidacyMessageRead(models.Model):
     candidacy = models.ForeignKey('candidacy.Candidacy', verbose_name=_('candidature'))
     user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('utilisateur'))
     is_read = models.BooleanField(_('est lu'), default=False)
     date = models.DateTimeField(_('date de dernière lecture'), auto_now=True)
+
+    class Meta:
+        verbose_name = _('message lu')
+        verbose_name_plural = _('messages lu')
+        unique_together = ('candidacy', 'user')
