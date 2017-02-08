ce59aea2e52490d4aa17f7d80fd923283d483dd8
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-08 16:58:06 +0000

    Updates CandidacyMessageRead for user when requesting all message

--- barneystinson/apps/message/api/views.py
+++ barneystinson/apps/message/api/views.py
@@ -1,10 +1,12 @@
 from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
 
+from django.utils import timezone
+
 from apps.notification import types
 from apps.notification.api.mixins import NotificationtMixin
 
-from ..models import CandidacyMessage
+from ..models import CandidacyMessage, CandidacyMessageRead
 from .serializers import CandidacyMessageSerializer
 
 
@@ -27,3 +29,11 @@ class CandidacyMessageViewSet(NotificationtMixin, ModelViewSet):
         elif self.request.user.is_applicant:
             qs_filter = {'candidacy__applicant': self.request.user.applicant}
         return CandidacyMessage.objects.filter(**qs_filter)
+
+    def list(self, request, *args, **kwargs):
+        response = super(CandidacyMessageViewSet, self).list(request, *args, **kwargs)
+        candidacy_id = self.request.GET.get('candidacy')
+        if candidacy_id:
+            qs_filter = {'user': self.request.user, 'candidacy_id': candidacy_id}
+            CandidacyMessageRead.objects.filter(**qs_filter).update(is_read=True, date=timezone.now())
+        return response
