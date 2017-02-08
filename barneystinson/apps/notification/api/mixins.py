9b1ba0c772dbd23e70f220470b7683581f0b1d55
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-08 15:50:49 +0000

    Adds an method to retrieve notification_type

--- barneystinson/apps/notification/api/mixins.py
+++ barneystinson/apps/notification/api/mixins.py
@@ -7,14 +7,17 @@ class NotificationtMixin(object):
     notification_type = None
     map_action_to_notification_type = None
 
-    def send_notification(self, instance, emitter):
+    def get_notification_type(self):
         if not self.notification_type and not self.map_action_to_notification_type:
             raise ImproperlyConfigured('"%s" should include a `notification_type` or `map_action_to_notification_type`'
                                        'attribute.' % self.__class__.__name__)
         if self.notification_type:
-            notification_type = self.notification_type
+            return self.notification_type
         else:
-            notification_type = self.map_action_to_notification_type.get(self.action)
+            return self.map_action_to_notification_type.get(self.action)
+
+    def send_notification(self, instance, emitter):
+        notification_type = self.get_notification_type()
         NotificationHandler(request=self.request, type_name=notification_type, emmiter=emitter,
                             action_object=instance).send()
 
