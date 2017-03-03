9edb80a3ae20eb1fb7a538fcc44aee5e64b048af
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-03-03 14:36:23 +0100

    Replaces action_object_id with action_object which returned value depending on is object + adds a new API endpoint to unread count

--- barneystinson/apps/notification/api/views.py
+++ barneystinson/apps/notification/api/views.py
@@ -1,14 +1,18 @@
+from rest_framework import decorators
 from rest_framework.mixins import ListModelMixin
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from apps.core.api.mixins import IsAuthenticatedMixin
 
 from ..models import Notification
+from .pagination import NotificationPagination
 from .serializers import NotificationSerializer
 
 
 class NotificationViewSet(IsAuthenticatedMixin, ListModelMixin, GenericViewSet):
     serializer_class = NotificationSerializer
+    pagination_class = NotificationPagination
     filter_fields = ('is_unread',)
 
     def get_queryset(self):
@@ -16,5 +20,11 @@ class NotificationViewSet(IsAuthenticatedMixin, ListModelMixin, GenericViewSet):
 
     def list(self, request, *args, **kwargs):
         response = super(NotificationViewSet, self).list(request, args, **kwargs)
-        Notification.objects.filter(receiver=request.user, is_unread=True).update(is_unread=False)
+        # Notification.objects.filter(receiver=request.user, is_unread=True).update(is_unread=False)
         return response
+
+    @decorators.list_route(methods=['get'], url_path='unread-count')
+    def unread_count(self, request, pk=None):
+        return Response({
+            'unread_count': Notification.objects.filter(receiver=self.request.user, is_unread=True).count(),
+        })
