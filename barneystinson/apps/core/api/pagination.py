9dd9decb257a9a566120663255add55d33d19aaf
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-03-13 16:43:46 +0100

    Removes useless import and adds __str__ where needed + try / catch urllib

--- barneystinson/apps/core/api/pagination.py
+++ barneystinson/apps/core/api/pagination.py
@@ -2,7 +2,11 @@
 from __future__ import unicode_literals, absolute_import
 
 from collections import OrderedDict
-from urllib.parse import urlparse, parse_qs
+
+try:
+    from urllib.parse import urlparse, parse_qs
+except ImportError:
+    from urlparse import urlparse, parse_qs
 
 from rest_framework.pagination import PageNumberPagination, CursorPagination
 from rest_framework.response import Response
--- barneystinson/apps/data/models.py
+++ barneystinson/apps/data/models.py
@@ -1,41 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from .behaviors import DataModel
 
 
-# @python_2_unicode_compatible
 class Industry(DataModel):
     class Meta(DataModel.Meta):
         verbose_name = _('Secteur d\'activité')
         verbose_name_plural = _('Secteurs d\'activité')
 
 
-# @python_2_unicode_compatible
 class Employee(DataModel):
     class Meta(DataModel.Meta):
         verbose_name = _('Nombre d\'employés')
         verbose_name_plural = _('Nombre d\'employés')
 
 
-# @python_2_unicode_compatible
 class ContractType(DataModel):
     class Meta(DataModel.Meta):
         verbose_name = _('Type de contrat')
         verbose_name_plural = _('Types de contrat')
 
 
-# @python_2_unicode_compatible
 class Experience(DataModel):
     class Meta(DataModel.Meta):
         verbose_name = _('Expérience')
         verbose_name_plural = _('Expérience')
 
 
-# @python_2_unicode_compatible
 class StudyLevel(DataModel):
     class Meta(DataModel.Meta):
         verbose_name = _('Niveau d\'étude')
